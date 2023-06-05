990091849c52762cb21057224167689a0bcb885b
Emilio Junior Corona <you@example.com>
2023-06-05 17:39:01 +0200

    esercizio 8 terminato

---
+++ script8.js
@@ -0,0 +1,45 @@
+
+// Es - 8
+
+// Dato un titolo e un bottone creare:
+
+// una funzione di javascript che al click sul bottone aggiunga e tolga una classe che cambi il colore
+
+
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <title>Document</title>
+//     <style>
+//       .color {
+//         color : red;
+//       }
+//     </style>
+//     </head>
+//     <body>
+//         <h1>Odio Javascript ! ... ma sempre meglio di PHP</h1>
+
+//     <script>
+
+//     </script>
+//     </body>
+//     </html>
+
+
+
+// let btnChangeColor = document.querySelector('.btnChangeColor')
+// let colorCustom = document.querySelector('.colorCustom')
+
+// btnChangeColor.addEventListener('click',() => {
+    
+//     colorCustom.classList.toggle('color')
+    
+
+// })
+
+
+
+
