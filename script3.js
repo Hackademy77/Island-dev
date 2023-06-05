e58eef6868175c3dc121e28e1ca199ffe0b3cd99
Emilio Junior Corona <you@example.com>
2023-06-05 16:05:55 +0200

    esercizio 4 finito

--- script3.js
+++ script3.js
@@ -17,16 +17,16 @@
 // Risultato: true  
 
 
-let list = [
-          { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, eyes: 'Brown' },
-          { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, eyes: 'Black' },
-          { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, eyes: 'Green' }
-        ];
+// let list = [
+//           { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, eyes: 'Brown' },
+//           { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, eyes: 'Black' },
+//           { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, eyes: 'Green' }
+//         ];
 
-        function eyes() {
-            let eyes = list.map((developer) => developer.eyes)
-            return (eyes.includes('Green'));
+//         function eyes() {
+//             let eyes = list.map((developer) => developer.eyes)
+//             return (eyes.includes('Green'));
             
-        }
-    console.log(eyes());
+//         }
+//     console.log(eyes());
         
--- script4.js
+++ script4.js
@@ -1,4 +1,3 @@
-
 // Es - 4 
 
 // Dato un Array di oggetti:
@@ -19,3 +18,26 @@
 // EXTRA
 
 // - Generalizza la funzione e fai scegliere all’utente il Linguaggio di programmazione.
+
+
+    // let list = [
+    //   { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+    //   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+    //   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+    // ];
+
+
+
+    // function findPython() {
+    //     let found = list.find((element) => element.language == 'Python')
+    //     if (found) {
+    //         return `${found.firstName} , ${found.country}`
+    //     } else {
+    //         return `Non ci sono sviluppatori Python`
+    //     }
+       
+         
+        
+    // }
+
+    // console.log(findPython());
