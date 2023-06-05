ba9ffe6d71ec08e831a33778ff6662be489e4ffa
Lanza Benedetto <bennylanza@gmail.com>
2023-06-05 17:20:18 +0200

    esercizio 7 finito

---
+++ script7.js
@@ -0,0 +1,45 @@
+
+// Es - 7
+
+// Dato un array contenente i giorni della settimana creare una funzione che inserisca all’interno del tag ul con id “openings” una lista non numerata con all’interno i giorni della settimana con gli orari delle aperture del negozio.
+
+
+//     Lunedi : dalle 9.00 alle 18.00
+//     Martedi : dalle 9.00 alle 18.00
+
+
+//     let openingDays =[
+//       {day : 'monday' , from : '9.00' , to : "18.00"},
+//       {day : 'tuesday' , from : '9.00' , to : "18.00"},
+//       {day : 'wednesday' , from : '9.00' , to : "18.00"},
+//       {day : 'thursday' , from : '9.00' , to : "18.00"},
+//       {day : 'saturday' , from : '9.00' , to : "18.00"},
+//       {day : 'sunday' , from : '9.00' , to : "18.00"},
+//     ]
+
+
+
+// let openingDays =[
+//           {day : 'monday' , from : '9.00' , to : "18.00"},
+//           {day : 'tuesday' , from : '9.00' , to : "18.00"},
+//           {day : 'wednesday' , from : '9.00' , to : "18.00"},
+//           {day : 'thursday' , from : '9.00' , to : "18.00"},
+//           {day : 'saturday' , from : '9.00' , to : "18.00"},
+//           {day : 'sunday' , from : '9.00' , to : "18.00"},
+//         ];
+
+//         let openings = document.querySelector('#openings')
+        
+
+//         openingDays.forEach((dayOpen) => {
+//             let li = document.createElement ('li')
+//             li.innerHTML = ` 
+//             ${dayOpen.day} , from ${dayOpen.from} , to ${dayOpen.to}`
+//             openings.appendChild(li)
+//          })
+       
+        
+      
+
+     
+    
