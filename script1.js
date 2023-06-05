04e3cbfc6bd4dad7ed95caead6e4faab80ecd074
Lanza Benedetto <bennylanza@gmail.com>
2023-06-05 15:15:56 +0200

    primo esercizio finito

--- script1.js
+++ script1.js
@@ -13,3 +13,21 @@
 // EXTRA
 
 // - Generalizza la funzione e fai scegliere all’utente il Continente e il Linguaggio di programmazione
+
+// let continent = prompt('Inserisci il continente')
+// let language = prompt('Inserisci il linguaggio')
+
+//     let list = [
+//       { firstName: 'Marco', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//       { firstName: 'Simone', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//       { firstName: 'Matteo', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//       { firstName: 'Ettore', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+//     ];
+
+//     function findDev(continent, language) {
+//         let filtered = list.filter((developer) => developer.continent.toLowerCase() == continent && developer.language.toLowerCase() == language)
+//         return filtered.length   
+      
+//     }
+
+//    console.log(findDev(continent , language))
