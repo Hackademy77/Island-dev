1645e8ed407f452e577f003165c818475c526ebd
Emilio Junior Corona <you@example.com>
2023-06-05 15:32:16 +0200

    esercizio 2 finito

--- script2.js
+++ script2.js
@@ -28,3 +28,26 @@
 //         greeting: 'Hi Madison, what do you like the most about Ruby?'
 //       } 
 //     ];
+
+
+
+
+//     let list = [
+//     { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+//     { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+//     { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+//     ];
+
+
+
+
+//     function greeting() {
+
+//         let newArray = list.map((developer) => developer.greeting = `Hi ${developer.firstName} , what do you like the most about ${developer.language} ?`)
+//         return newArray
+        
+//     }
+
+
+//    console.log(greeting());
+//    console.log(list);
