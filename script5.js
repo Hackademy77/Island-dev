a8d60154da7136b2657e2118b185d482c9e072a1
Lanza Benedetto <bennylanza@gmail.com>
2023-06-05 16:34:12 +0200

    esercizio 5 finito

--- script5.js
+++ script5.js
@@ -13,3 +13,28 @@
 //     ];
 // - Utilizzare alert() → metodo che fa visualizzare una finestra di pop-up per mostrare un messaggio di avviso all’utente
 //     alert('Messaggio di avviso per l'utente');
+
+
+// let list = [
+//             { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, logged: true },
+//             { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, logged: false },
+//             { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, logged: true }
+//         ];
+//         let name1 = prompt('Inserisci un nome')
+//         function logged(name) {
+//             let confirm = false
+//             list.map((person) => {
+//                 if(person.firstName == name && person.logged == true) {
+//                   confirm = true 
+//                 }
+//             })
+//             if (confirm == true) {
+//                 alert("L'utente è stato registrato")
+
+//             }else {
+//                 alert("L'utente non è stato registrato")
+
+//             }
+//         }
+//         logged(name1)
+
